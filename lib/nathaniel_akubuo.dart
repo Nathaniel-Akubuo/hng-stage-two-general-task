2a5194ad2536d38219056283909ba9a43d387fbe
Nathaniel Akubuo <nakubuo@gmail.com>
2021-08-17 20:30:34 +0100

    latest changes

---
+++ lib/nathaniel_akubuo.dart
@@ -0,0 +1,3 @@
+void main() {
+  print('Nathaniel Akubuo');
+}
--- src/com/akubuo/Main.java
+++
@@ -1,8 +0,0 @@
-package com.akubuo;
-
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("Nathaniel Akubuo");
-    }
-}
